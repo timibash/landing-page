b71bf3b49958b9697fb337972fb065678730a0cf
timibash <85651144+timibash@users.noreply.github.com>
2023-08-08 08:12:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const returnHome = () => {
     window.location.href = "/";
 };
 
-const HamburgerMenBtn = document.querySelector('.HamburgerMenu');
-const navlinks = document.querySelector(".hideshow2");
+const hamburgerMenuBtn = document.querySelector('.HamburgerMenu'); 
+const navLinks = document.querySelector(".hideshow2");
 const header = document.querySelector(".head");
 const frame = document.querySelector(".frame");
-const CloseIconBtn = document.querySelector(".last");
-const allNavlinks = document.querySelectorAll(".a");
-
+const closeIconBtn = document.querySelector(".last"); 
+const allNavLinks = document.querySelectorAll(".a");
 
 const showNavLinks = () => {
-      
-    header.classList.toogle("head2");
-    frame.classList.toogle("show2");
-    HamburgerMenBtn.classList.toogle("active");
+    header.classList.toggle("head2"); 
+    frame.classList.toggle("show2"); 
+    hamburgerMenuBtn.classList.toggle("active"); 
+    
+    
+};
+
+hamburgerMenuBtn.addEventListener('click', showNavLinks);
+
 
 
     
